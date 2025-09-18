ec5e45312a2606f830f39c663ab95a2daba8593b
Prajwal Patil <73753614+Prajwal-glitch@users.noreply.github.com>
2025-09-18 22:13:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ st.title("Fruit Freshness Detection")
 uploaded_file = st.file_uploader("Upload the file", type=["jpg", "png"])
 
 if uploaded_file:
-    image_path = "temp_file.jpg"
-    with open(image_path, "wb") as f:
-        f.write(uploaded_file.getbuffer())
-        st.image(uploaded_file, caption="Uploaded File", width=300)
-        prediction = predict(image_path)
-        st.info(f"Predicted Class: {prediction}")
+    #image_path = "temp_file.jpg"
+    #with open(image_path, "wb") as f:
+        #f.write(uploaded_file.getbuffer())
+    st.image(uploaded_file, caption="Uploaded File", width=300)
+    prediction = predict(image_path)
+    st.info(f"Predicted Class: {prediction}")
